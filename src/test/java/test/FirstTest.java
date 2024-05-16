5d51988f040cb22c43bae588df3e39d2b3e8d197
OleksandrKupchuk <55634677+OleksandrKupchuk@users.noreply.github.com>
2024-05-16 19:10:59 +0300

    some commit

--- src/test/java/test/FirstTest.java
+++ src/test/java/test/FirstTest.java
@@ -1,11 +1,20 @@
 package test;
 
 import com.codeborne.selenide.Selenide;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 import org.testng.annotations.Test;
 
 public class FirstTest {
     @Test(groups = {"release"})
     public void firstTest(){
         Selenide.open("https://the-internet.herokuapp.com/");
+    }
+
+    @Test(groups = {"playwright"})
+    public void secondTest(){
+        Page page = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(true)).newPage();
+        page.navigate("https://the-internet.herokuapp.com/");
     }
 }
